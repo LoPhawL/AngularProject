1f19b4a918b38d33693743354dd859efe3aa184f
staunchers <r.harip3@gmail.com>
2020-05-01 12:49:49 +0530

    reOrg

--- src/app/Modules/recipes/Recipe.Module.ts
+++ src/app/Modules/recipes/Recipe.Module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RecipesComponent } from './Components/recipes.component';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeListComponent } from './Components/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './Components/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipeItemComponent } from './Components/recipe-list/recipe-item/recipe-item.component';
 import { RecipeStartComponent } from './Components/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './Components/recipe-edit/recipe-edit.component';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AuthGuard } from 'src/app/ServiceDependencies/AuthGuard.Service';
+import { AuthGuard } from '../../ServiceDependencies/AuthGuard.Service';
 
 
 @NgModule(
--- src/app/Modules/shopping-list/ShoppingList.Module.ts
+++ src/app/Modules/shopping-list/ShoppingList.Module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
-import { ShoppingListComponent } from './shopping-list.component';
-import { ShoppingListEditComponent } from './shopping-list-edit/shopping-list-edit.component';
+import { ShoppingListComponent } from './Components/shopping-list.component';
+import { ShoppingListEditComponent } from './Components/shopping-list-edit/shopping-list-edit.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
